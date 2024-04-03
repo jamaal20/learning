ae8d243ded628433567b3c3ac99722e3179a7370
jamaal20 <jamydeen11@gmail.com>
2024-04-03 10:04:12 -0700

    first git commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const name ="jamaal arun";
+console.log(`Hello ${name}`);
+const school = "universityof california, Berkely";
+console.log(`school : ${school}`);
+let year = 3;
+year = year + 1;
+console.log (`I am in year: ${year}`)
+
